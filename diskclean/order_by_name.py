c64358dee569e00bf36a18e6e4cc48cab0687247
Martin Hvidberg <martin@hvidberg.net>
2018-03-03 09:45:34 +0100

    order by name, operational

--- diskclean/order_by_name.py
+++ diskclean/order_by_name.py
@@ -1,13 +1,43 @@
 import os
 import pickle
 
+str_ossep = "/"
 str_order = "/home/output/.TMP/NAM" # """./"
 str_chaos = "/home/output/.TMP/NEWS" # """./"
 str_fn = 'dic_bab.ecp'
 
 with open(str_fn, 'rb') as handle:
-    dic_bab = = pickle.load(handle)
+    dic_bab = pickle.load(handle)
 
+lst_suc = list()
 for dirpath, dnames, fnames in os.walk(str_chaos):
     for f in fnames:
         for hit in dic_bab:
+            for pat in dic_bab[hit]:
+                if all([tag in f for tag in pat]):
+                    print "Hit: [{}] {} > {}".format(hit, pat, f)
+                    lst_suc.append((hit, pat, (os.path.join(dirpath, f)), f.replace(' ','_')))
+lst_suc.sort()
+
+# Move
+for suc in lst_suc:
+    print "\n",suc
+    str_old = suc[2]
+
+    # Check dir exists, or make
+    str_dir = str_order+str_ossep+suc[0]
+    if not os.path.exists(str_dir):
+        os.makedirs(str_dir)
+
+    # Check file not exists
+    str_new = str_dir+str_ossep+suc[3]
+    print str_new
+    if not os.path.isfile(str_new):
+
+        # Rename
+        try:
+            os.rename(str_old, str_new)
+            print "moved: {}".format(suc[3])
+        except:
+            pass
+
