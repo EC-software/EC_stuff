6b68bf11ab5bfe011cb9efdeefb8715961c955f0
Martin Hvidberg <martin@hvidberg.net>
2018-03-28 20:07:58 +0200

    ECPWB sems operational, and solid, thought not entierly complete...

--- ec_base/ecpasswordbase.py
+++ ec_base/ecpasswordbase.py
@@ -2,8 +2,8 @@
 # -*- coding: ascii -*-
 
 """
-Storing identity usernames, passwords and other info in a safe place, away from your repository and work-dir,
-so it won't accedentially sync to bitbucket, github, dropbox or similar
+Storing identity user-names, passwords and other info in a safe place, away from your repository and work-dir,
+so it won't accidentally sync to bitbucket, github, dropbox or similar
 Usage:
     1) ecpwb = ecpasswordbase.Base('/home/myuser/personalstuff/my_sectres.ecpwb')
     2) the_username = ecpwb.get('identity_id', 'user')
@@ -57,7 +57,7 @@ class Base(object):
             str_ret = fil.read()
         ret_load = json.loads(str_ret)
         if ret_load == dic_new:
-            print "JSON is identical"
+            print "JSON Init is identical"
         else:
             print "!!! Error on reload: JSON"
             if json_load == str_ret:
@@ -84,10 +84,10 @@ class Base(object):
                             # Load the data
                             self._version = dic_load['version']
                             self._filen = dic_load['filen']
-                            self._crypt = dic_load['filen']  # No encryption supported, yet
-                            self._dicdb = dic_load['filen']
-                            self._empty = dic_load['filen']
-                            self._valid = dic_load['filen']
+                            self._crypt = dic_load['crypt']  # No encryption supported, yet
+                            self._dicdb = dic_load['dicdb']
+                            self._empty = dic_load['empty']
+                            self._valid = dic_load['valid']
                         else:
                             print "{} Can't load file: {} since it's version {}".format(__file__, self._filen, dic_load['version'])
                             return 206
@@ -109,19 +109,95 @@ class Base(object):
         """ Validate the self objects with some sanity checks """
         return 0
 
-    def add(self):
-        """ Add an identity to an existing .ecpwb file """
+    def _upd(self):
+        """ Update an existing .ecpwb file, with current self._dicdb """
+        self._validate()
+        if self._valid:
+            dic_upd = dict()
+            dic_upd['datatype'] = "ECPWB"
+            dic_upd['version'] = self._version
+            dic_upd['filen'] = self._filen
+            dic_upd['crypt'] = self._crypt
+            dic_upd['dicdb'] = self._dicdb
+            dic_upd['empty'] = self._empty
+            dic_upd['valid'] = self._valid
+            json_load = json.dumps(dic_upd, sort_keys=True, indent=4)
+            with open(self._filen, "w") as fil:
+                fil.write(json_load)
+            with open(self._filen, "r") as fil:
+                str_ret = fil.read()
+            ret_load = json.loads(str_ret)
+            if ret_load == dic_upd:
+                print "JSON Update is identical"
+            else:
+                print "!!! Error on reload: JSON"
+                if json_load == str_ret:
+                    print "    but json == json, so it's likely just caused by non-string keys in input"
+                return 230
+
         return 0
 
-    def get(self):
+    def add(self, str_key, dic_add):
+        """ Add an identity to an existing .ecpwb file
+        the identity must be in form of a dictionary, with all string keys.
+        and the identity-key cant be on all ready used in self."""
+        if isinstance(str_key, str):
+            if isinstance(dic_add, dict):
+                if not str_key in self._dicdb.keys():
+                    if all([isinstance(key_i, str) for key_i in dic_add.keys()]):
+                        self._dicdb[str_key] = dic_add
+                    else:
+                        print "Can't add identity because it contains key(s) that are not string"
+                        return 224
+                else:
+                    print "Can't add identity because key all readay exists: {}".format(str_key)
+                    return 223
+            else:
+                print "Can't add identity because it's not a dictionary, its: {}".format(str(type(dic_add)))
+                return 222
+        else:
+            print "Can't add identity because the key in not string, its: {}".format(str(type(str_key)))
+            return 221
+        self._upd()
+        return 0
+
+    def get(self, str_key, str_inner_key):
         """ Retrieve an identity from an existing .ecpwb file """
-        return 0
+        if isinstance(str_key, str):
+            if isinstance(str_inner_key, str):
+                if str_key in self._dicdb.keys():
+                    if str_inner_key in self._dicdb[str_key].keys():
+                        return self._dicdb[str_key][str_inner_key]
+                    else:
+                        print "Can't find key: {} in {}".format(str_inner_key, str_key)
+                        return None
+                else:
+                    print "Can't find key: {}".format(str_key)
+                    return None
+            else:
+                print "None-string type in key: {} in {}".format(str_inner_key, str_key)
+                return None
+        else:
+            print "None-string type in key: {}".format(str_key)
+            return None
 
-    def rem(self):
+
+    def set(self, str_key, str_inner_key, inner_val):
+        """ Write (overwrite if exists) an identity to an existing .ecpwb file """
+        dic_set = self.get(str_key, str_inner_key)
+        if isinstance(str_inner_key, str):
+            dic_set[str_inner_key] = inner_val
+            self._upd()
+            return 0
+        else:
+            print "None-string type in key: {} in {}".format(str_inner_key, str_key)
+            return 240
+
+    def rem(self, str_key):
         """ Remove an identity from an existing .ecpwb file """
-        return 0
-
-    def upd(self):
-        """ Update an identity in an existing .ecpwb file """
-        return 0
-
+        if str_key in self._dicdb.keys():
+            del self._dicdb[str_key]
+            self._upd()
+            return 0
+        else:
+            print "Can't remove key: {}, because it's not there...".format(str_key)
--- ec_base/ecpasswordbase_test.py
+++ ec_base/ecpasswordbase_test.py
@@ -2,4 +2,8 @@
 import ecpasswordbase
 
 ecp = ecpasswordbase.Base(r"/home/martin/Private/Secrets.ecpwb")
-
+ecp.add('homeDB', {'name': 'myuser', 'pasw': 'verysecret', 'port': 1234})
+my_pw_back = ecp.get('homeDB', 'pasw')
+print my_pw_back
+ecp.rem('workDB')
+ecp.rem('homeDB')
