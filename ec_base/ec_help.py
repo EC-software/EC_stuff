f2dff05e57fe4b2c0d919e2bd94f2cc6e3eb36b4
Martin Hvidberg <martin@hvidberg.net>
2018-03-30 16:09:24 +0200

    PAWND check running, fast... :-)

--- ec_base/ec_help.py
+++ ec_base/ec_help.py
@@ -10,8 +10,20 @@ class ec_cntdic(object):
         else:
             self.data[itm] += 1
 
+    def sub(self, itm):
+        if itm not in self.data.keys():
+            self.data[itm] = -1
+        else:
+            self.data[itm] -= 1
+
     def by_cnt(self, sort=True):
         if sort:
             return sorted([(self.data[key], key) for key in self.data.keys()], reverse=True)
         else:
             return [(self.data[key], key) for key in self.data.keys()]
+
+    def by_key(self, sort=True):
+        if sort:
+            return sorted([(key, self.data[key]) for key in self.data.keys()])
+        else:
+            return [(key, self.data[key]) for key in self.data.keys()]
--- passw_leak/src/big_file_splitter.py
+++ passw_leak/src/big_file_splitter.py
@@ -1,25 +1,41 @@
 import datetime
 
+bol_small_files = False
+bol_cnt_hex_3 = False
+bol_print_one = True
+
+# Prep. the dic
 dic_temp = dict()
+lst_chrs = "0123456789abcdef"
+lst_lbls = [a+b+c for c in lst_chrs for b in lst_chrs for a in lst_chrs]
+ecc = dict()
+for labl in lst_lbls:
+    dic_temp[labl] = 0
 
 with open('/home/martin/Downloads/pwned/pwned-passwords-2.0.txt', 'r') as fin:
     num_cnt = 0
     for line in fin:
-        str_key = line[:3]
-        if not str_key in dic_temp.keys():
-            dic_temp[str_key] = list()
-        dic_temp[str_key].append(line.strip())
+        line = line.lower()
         num_cnt += 1
         if num_cnt % 100000 == 0:
             print "{} dumping at: {}".format(datetime.datetime.now(),num_cnt)
-            #for keyt in dic_temp:
-                #print keyt, dic_temp[keyt]
-                #filt = open(r"../data/pwn_{}.txt".format(keyt), 'w+')
-                #filt.write()
+        if bol_small_files:  # Split in small files
+            str_key = line[:3]
+            if not str_key in dic_temp.keys():
+                dic_temp[str_key] = list()
+            dic_temp[str_key].append(line.strip())
             for str_tmp in dic_temp:
                 with open("/home/martin/Downloads/pwned/split4096/pwnd_{}.txt".format(str_tmp), 'a') as the_file:
                     for lint in dic_temp[str_tmp]:
                         the_file.write(lint+'\n')
             dic_temp = dict()
+        if bol_cnt_hex_3:  # Count the keys
+            dic_temp[line[:3]] += 1
+        if bol_print_one:  # Simply print any line with this key...
+            str_key = line[:3]
+            if str_key.lower() == 'fff':
+                print line.strip()
 
+if bol_cnt_hex_3:
+    print [(k, dic_temp[k]) for k in sorted(dic_temp)]
 
