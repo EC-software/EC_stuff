e480c756c044046f0e2c14dcc21d770bca4ed31c
M0LLY <m0lly.roberts.liverpool@gmail.com>
2023-06-29 19:46:37 +0200

    Golden_triangle::: reload, all 2 of 3 methods implemented SEQ20230629EOD

--- golden_triangle/reload.py
+++ golden_triangle/reload.py
@@ -6,21 +6,53 @@
 
 import os
 import shutil
+import stat
 
 KNOWN_METHODS = ["os.", "shutil.", "cmd"]
 
 
 def movefile_byos(src, dst):
     print(f"Move: {src} --> {dst}, by os.")
+    try:
+        with open(src, 'br') as fil_in:
+            with open(dst, 'bw') as fil_ou:
+                fil_ou.write(fil_in.read())
+    except:
+        print(f"Can't copy file with os.: {src} --> {dst}")
+        return 101
+    os.remove(src)
 
 
 def movefile_byshutil(src, dst):
     print(f"Move: {src} --> {dst}, by .shutil")
-    shutil.copyfile(src, dst)
+    try:
+        shutil.copyfile(src, dst)
+    except OSError:
+        return 201
+    except:
+        print(f"Can't move file with shutil: {src} --> {dst}")
+        return 202
+    os.remove(src)
 
 
 def movefile_bycmd(src, dst):
     print(f"Move: {src} --> {dst}, by cmd")
+    str_osname = os.name
+    if str_osname == 'posix':  # Linux ----------------------------------------
+        str_tmpfilename = r"__temp__file__.sh"
+        with open(str_tmpfilename, 'w') as fil_cmd:
+            fil_cmd.write(f"cp {src} {dst}\nrm {src}\n")
+        os.chmod(str_tmpfilename, stat.S_IXUSR)
+        os.system(str_tmpfilename)
+        os.remove(str_tmpfilename)
+    elif str_osname == 'nt':  # windows ---------------------------------------
+        pass
+    elif str_osname == 'java':  # Java ----------------------------------------
+        print(f"Warning: movefile_bycmd() detects UNSUPPORTED OS: {str_osname}")
+        return 302
+    else:  # ------------------------------------------------------------------
+        print(f"Warning: movefile_bycmd() detects unknown OS: {str_osname}")
+        return 301
 
 
 def moveall(str_fdn_from, str_fdn_to, method="none", ttt=3):
