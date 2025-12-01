a466bf3a7b0429f08460a66114e5edde86aa6ac9
martin <martin@hvidberg.net>
2025-12-01 20:01:52 +0100

    SEQ202512012000d

--- ecpw/ecpw_demo.py
+++ ecpw/ecpw_demo.py
@@ -5,10 +5,10 @@
 EC-software
 This small file demonstrates the typical everyday use of ecpw
 
-Line 10: import the ecpw module
-Line 11: create the ecpw object - this will automatically read your data from file
-Line 13: get the 3 values, that we need for this program
-Line 15: print the values, so we can verify that it worked. Almost too easy...
+Line 14: import the ecpw module
+Line 15: create the ecpw object - this will automatically read your data from file
+Line 18: get the 3 values, that we need for this program
+Line 20: print the values, so we can verify that it worked. Almost too easy...
 """
 
 import ecpw
@@ -16,4 +16,5 @@ ecs = ecpw.Store()
 
 entry = 'demo_db'
 db_ip, db_name, db_pw = ecs.gets(entry, ['ip', 'name', 'password'])
+
 print(f"{entry} : {db_ip}, use credentials: {db_name} / {db_pw}")
