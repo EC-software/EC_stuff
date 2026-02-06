067e977a5921109293bec6c6113d2e92b355aae4
Martin Hvidberg <Martin@Hvidberg.net>
2026-02-06 15:10:15 +0100

    SEQ20260106

--- misc/filter_lines.py
+++ misc/filter_lines.py
@@ -1,17 +1,20 @@
 import os
 import sys
 
-rootdir = sys.argv[1]
-target = "Rota"
+rootdir = r"C:\Users\22016\Martin\repos"  # sys.argv[1]
+target = "Music"
 
 print(rootdir)
 with open("filter_lines_out", "w") as filo:
     for subdir, dirs, files in os.walk(rootdir):
         for file in files:
             filepath = subdir + os.sep + file
-            if filepath.endswith(".csv"):
+            if filepath.endswith(".py") and not any([filepath.find(tok) > 0 for tok in ['venv']]):
                 print(filepath)
                 with open(filepath, "r") as fili:
-                    for line in fili:
-                        if target.lower() in line.lower():
-                            filo.write(str(filepath)+"<<"+line)
+                    try:
+                        for line in fili:
+                            if target.lower() in line.lower():
+                                filo.write(str(filepath)+"<<"+line)
+                    except UnicodeDecodeError:
+                        pass
